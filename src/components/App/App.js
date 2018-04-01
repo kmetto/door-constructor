510d41ecd9c6400debd2f63d791a205e3ae95439
akiyamka <akiyamka@gmail.com>
2018-04-01 16:21:48 +0300

    theme css

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,6 +5,8 @@ import './App.css';
 import data from '../../data';
 import DoorSeries from '../DoorSeries/DoorSeries';
 import InstallationType from '../InstallationType/InstallationType';
+import PenType from '../PenType/PenType';
+import LockType from '../LockType/LockType';
 import CurrentCode from '../../containers/CurrentCode/CurrentCode';
 
 export default class App extends Component<{}> {
@@ -12,8 +14,38 @@ export default class App extends Component<{}> {
     return (
       <section className="app-base-grid">
         <aside>
-          <DoorSeries series={data.doorSeries}/>
-          <InstallationType installTypes={data.installing} />
+          <div className="fieldset series">
+            <legend>Серия дверей</legend>
+            <DoorSeries series={data.doorSeries}/>
+          </div>
+
+          <div className="fieldset design">
+            <legend>Дизайн</legend>
+            <InstallationType installTypes={data.installing} />
+          </div>
+
+          <div className="fieldset fittings">
+            <legend>Фурнитура</legend>
+            <LockType installTypes={data.installing} />
+            <PenType installTypes={data.installing} />
+            <PenType installTypes={data.installing} />
+          </div>
+
+          <div className="fieldset material">
+            <legend>Материал и цвет</legend>
+            <InstallationType installTypes={data.installing} />
+          </div>
+
+          <div className="fieldset box">
+            <legend>Коробка</legend>
+            <InstallationType installTypes={data.installing} />
+          </div>
+
+          <div className="fieldset size">
+            <legend>Размеры</legend>
+            <InstallationType installTypes={data.installing} />
+          </div>
+
         </aside>
         <section>
           <CurrentCode />
